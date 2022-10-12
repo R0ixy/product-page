1613510e6a59a8a6fdff68140c2ce502c74e7d74
R0ixy <nikita6kom@gmail.com>
2022-10-12 18:09:07 +0300

    implement arrows for mobile slider

--- src/components/product-page/images-slider/images-slider.tsx
+++ src/components/product-page/images-slider/images-slider.tsx
@@ -13,13 +13,18 @@ import productPreview2 from '@assets/images/image-product-2-thumbnail.jpg';
 import productPreview3 from '@assets/images/image-product-3-thumbnail.jpg';
 import productPreview4 from '@assets/images/image-product-4-thumbnail.jpg';
 
+import iconPrevious from '@assets/images/icon-previous.svg';
+import iconNext from '@assets/images/icon-next.svg';
+
 const products = {
   [productPreview1]: product1,
   [productPreview2]: product2,
   [productPreview3]: product3,
   [productPreview4]: product4,
 };
+
 const previews = Object.keys(products);
+const productsArray = Object.values(products);
 
 const ImagesSlider: FC = () => {
   const [activeImage, setActiveImage] = useState(product1);
@@ -28,8 +33,28 @@ const ImagesSlider: FC = () => {
     setActiveImage(products[preview]);
   };
 
+  const previousClickHandler = () => {
+    setActiveImage((prev) => productsArray[productsArray.indexOf(prev) - 1] || productsArray.at(-1));
+  };
+
+  const nextClickHandler = () => {
+    setActiveImage((prev) => productsArray[productsArray.indexOf(prev) + 1] || productsArray[0]);
+  };
+
   return (
     <div className={styles.sliderWrapper}>
+      <img
+        className={styles.imageArrowLeft}
+        src={iconPrevious}
+        alt="previous"
+        onClick={previousClickHandler}
+      />
+      <img
+        className={styles.imageArrowRight}
+        src={iconNext}
+        alt="next"
+        onClick={nextClickHandler}
+      />
       <img className={styles.activeImage} src={activeImage} alt="active product"/>
 
       {previews.map((product, index) =>
