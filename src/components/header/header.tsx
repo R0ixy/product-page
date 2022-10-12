8aa0a8d109d070350c1b9d131617ca522ac8df85
R0ixy <nikita6kom@gmail.com>
2022-10-12 11:36:16 +0300

    make header adaptive

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,15 +1,27 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import clsx from 'clsx';
 
 import styles from './styles.module.scss';
-import iconCart from '@assets/images/icon-cart.svg';
+import CartIcon from '@assets/images/icon-cart.svg';
 import avatar from '@assets/images/image-avatar.png';
+import menuIcon from '@assets/images/icon-menu.svg';
+import closeIcon from '@assets/images/icon-close.svg';
 
 const Header: FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  }
+
   return (
     <div className={styles.headerWrapper}>
       <div>
+        <img className={styles.menuIcon} src={menuIcon} alt="menu" onClick={toggleMenu}/>
         <span className={styles.logo}>sneakers</span>
-        <div className={styles.menu}>
+        {isMenuOpen && <div className={styles.background} onClick={toggleMenu}/>}
+        <div className={clsx(styles.menu, isMenuOpen && styles.showMenu)}>
+          <img className={styles.closeIcon} src={closeIcon} alt="close" onClick={toggleMenu}/>
           <span className={styles.menuTitle}>Collections</span>
           <span className={styles.menuItem}>Men</span>
           <span className={styles.menuItem}>Women</span>
@@ -18,7 +30,7 @@ const Header: FC = () => {
         </div>
       </div>
       <div className={styles.rightIcons}>
-        <img className={styles.cart} src={iconCart} alt="cart"/>
+        <img className={styles.cart} src={CartIcon} alt="cart"/>
         <img className={styles.avatar} src={avatar} alt="avatar"/>
       </div>
 
